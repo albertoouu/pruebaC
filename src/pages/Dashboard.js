92aae580ab389a1e41e6757374360edaafadeda6
albertoouu <albertoouu@gmail.com>
2022-03-23 22:27:00 -0600

    add react-big-calendar

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,7 +1,54 @@
-import React from 'react'
+import { useState, useEffect } from 'react'
 import { Link, Outlet } from 'react-router-dom'
+import FullCalendar from '@fullcalendar/react'
+import dayGridPlugin from '@fullcalendar/daygrid'
+import interactionPlugin from '@fullcalendar/interaction'
+import { initializeApp } from 'firebase/app'
+import { getFirestore, addDoc, collection, query, onSnapshot, orderBy, startAt, endAt } from 'firebase/firestore'
+import firebaseConfig from '../config'
+const app = initializeApp(firebaseConfig)
+const db = getFirestore(app)
 
+const handleClick = e => {
+  const title = prompt('enter a title', e.dateStr)
+  const event = {
+    title: title ? title : e.dateStr,
+    start: e.date,
+    allDay: true
+  }
+  addDoc(collection(db, 'CalendarTest'), event)
+}
 const Dashboard = () => {
+
+  const [data, setData] = useState([])
+  const [range, setRange] = useState({
+    start: new Date(),
+    end: new Date()
+  })
+  useEffect(() => {
+    //mounts
+    const q = query(collection(db, 'CalendarTest'), orderBy('start'), startAt(range.start), endAt(range.end))
+    const unsub = onSnapshot(q, (snap) => {
+      const array = snap.docs.map(doc => {
+        return {
+          id: doc.id,
+          title: doc.get('title'),
+          start: doc.get('start').toDate(),
+          allDay: doc.get('allDay')
+        }
+      })
+      console.log(array)
+      setData([...array])
+    })
+    //unmounts
+    return () => { unsub() }
+  }, [range])
+
+  const handleDatesSet = (e) => {
+    console.log(e)
+    setRange({ start: e.start, end: e.end })
+  }
+
   return (
     <>
       <Link to='comunity'>Comunidad</Link>
@@ -9,6 +56,12 @@ const Dashboard = () => {
       <Link to='form'>formulario</Link>
       <div>Dashboard</div>
       <Outlet />
+      <FullCalendar
+        events={data}
+        plugins={[dayGridPlugin, interactionPlugin]}
+        dateClick={handleClick}
+        datesSet={handleDatesSet}
+      />
     </>
   )
 }
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,8 +1,72 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { initializeApp } from 'firebase/app'
+import { getFirestore, addDoc, collection, query, onSnapshot, orderBy, startAt, endAt } from 'firebase/firestore'
+import firebaseConfig from '../config'
+import 'react-big-calendar/lib/css/react-big-calendar.css'
+import { Calendar, momentLocalizer } from 'react-big-calendar'
+import moment from 'moment'
+require("moment/locale/es-mx")
+const localizer = momentLocalizer(moment)
+const app = initializeApp(firebaseConfig)
+const db = getFirestore(app)
+
+const handleClick = e => {
+  const title = prompt('enter a title', e.start)
+  const event = {
+    title: title ? title : e.start,
+    start: e.start,
+    end: e.start,
+    allDay: true
+  }
+  addDoc(collection(db, 'CalendarTest'), event)
+}
+
+
 
 const HomePage = () => {
+
+  const [data, setData] = useState([])
+  const [range, setRange] = useState({
+    start: new Date(),
+    end: new Date()
+  })
+
+  useEffect(() => {
+    //mounts
+    const q = query(collection(db, 'CalendarTest'), orderBy('start'), startAt(range.start), endAt(range.end))
+    const unsub = onSnapshot(q, (snap) => {
+      const array = snap.docs.map(doc => {
+        return {
+          id: doc.id,
+          title: doc.get('title'),
+          start: doc.get('start').toDate(),
+          end: doc.get('end').toDate(),
+          allDay: doc.get('allDay')
+        }
+      })
+      console.log(array)
+      setData([...array])
+    })
+    //unmounts
+    return () => { unsub() }
+  }, [range])
+
+  const handleRangeChange = e => {
+    setRange({ start: e.start, end: e.end })
+  }
+
   return (
-    <div>HomePage</div>
+    <>
+      <div>HomePage</div>
+      <Calendar
+        localizer={localizer}
+        events={data}
+        style={{ height: 500, margin: "50px" }}
+        onSelectSlot={handleClick}
+        selectable
+        onRangeChange={handleRangeChange}
+      />
+    </>
   )
 }
 
