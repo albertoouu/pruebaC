035459000ac389554996b1301912b7f1a0c00cdc
albertoouu <albertoouu@gmail.com>
2022-03-30 01:33:34 -0600

    create new payday

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,23 +1,52 @@
-import { Button } from "react-bootstrap";
-import { Modal } from "react-bootstrap";
+import { useState } from "react";
+import { Button, Modal, Form } from "react-bootstrap";
 
+const ModalU = ({ show, setShow, modalUserInfo }) => {
 
-const ModalU = ({ show, setShow }) => {
   const handleClose = () => setShow(false);
+  const [date, setDate] = useState({})
+
+
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    const timeStamp = Date.parse(date) + 107999000
+    console.log(timeStamp)
+    if (!timeStamp) {
+      alert('Ingrese una fecha')
+      return
+    }
+    const nuevaFecha = new Date(timeStamp) //La nueva fecha de pago
+    console.log(nuevaFecha)
+    setDate({})
+    handleClose()
+  }
 
   return (
     <>
       <Modal show={show} onHide={handleClose}>
         <Modal.Header closeButton>
-          <Modal.Title>Modal heading</Modal.Title>
+          <Modal.Title>{modalUserInfo.title}</Modal.Title>
         </Modal.Header>
-        <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
+        <Modal.Body>
+          <p>Ultimo Pago: {modalUserInfo.strPayday}</p>
+          <p>Proximo pago: {modalUserInfo.strNext_payday}</p>
+          <Form>
+            <Form.Group>
+              <Form.Label>Actualizar pago</Form.Label>
+              <Form.Control type="date" required id="date" onChange={e => setDate(e.target.value)} />
+              <Form.Text className="text-muted">
+                Actualiza la fecha de pago
+              </Form.Text>
+            </Form.Group>
+            <Button variant="danger" type="submit" onClick={handleSubmit}>
+              Actualizar
+            </Button>
+          </Form>
+        </Modal.Body>
         <Modal.Footer>
-          <Button variant="secondary" onClick={handleClose}>
-            Close
-          </Button>
           <Button variant="primary" onClick={handleClose}>
-            Save Changes
+            Cerrar
           </Button>
         </Modal.Footer>
       </Modal>
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,29 +1,39 @@
 import React, { useEffect, useState } from 'react'
 import { initializeApp } from 'firebase/app'
-import { getFirestore, addDoc, collection, query, onSnapshot } from 'firebase/firestore'
+import { getFirestore, collection, query, onSnapshot } from 'firebase/firestore'
 import firebaseConfig from '../config'
 import 'react-big-calendar/lib/css/react-big-calendar.css'
 import { Calendar, momentLocalizer } from 'react-big-calendar'
 import moment from 'moment'
+import ModalU from '../components/Modal'
 require("moment/locale/es-mx")
 const localizer = momentLocalizer(moment)
 const app = initializeApp(firebaseConfig)
 const db = getFirestore(app)
 
-const handleClick = e => {
-  const title = prompt('enter a title', e.start)
-  const event = {
-    title: title ? title : e.start,
-    start: e.start,
-    end: e.start,
-    allDay: true
-  }
-  addDoc(collection(db, 'CalendarTest'), event)
-}
+
+
 
 const HomePage = () => {
 
   const [data, setData] = useState([])
+  const [users, setUsers] = useState([])
+  const [modalUserInfo, setModalUserInfo] = useState({})
+  const [show, setShow] = useState(false);
+  const handleShow = () => setShow(true);
+
+  const handleClick = e => {
+    const user = users.filter(u => {
+      if (e.id === u.id) return true
+    })
+    setModalUserInfo({
+      title: user[0].title,
+      strPayday: user[0].start.toDateString(),
+      strNext_payday: user[0].nextPayDay.toDateString(),
+      id: user[0].id
+    })
+    handleShow()
+  }
 
   useEffect(() => {
     //mounts
@@ -33,32 +43,46 @@ const HomePage = () => {
         return {
           id: doc.id,
           title: doc.get('name'),
-          start: doc.get('date_start').toDate(),
-          end: doc.get('date_start').toDate(),
+          start: doc.get('payday').toDate(),
+          nextPayDay: doc.get('next_payday').toDate(),
+          end: doc.get('payday').toDate(),
           allDay: true
         }
       })
-      setData([...array])
+      const payDays = snap.docs.map(doc => {
+        return {
+          id: doc.id,
+          title: doc.get('name'),
+          start: doc.get('next_payday').toDate(),
+          end: doc.get('next_payday').toDate(),
+          allDay: true
+        }
+      })
+      const allDays = array.concat(payDays)
+      setUsers([...array])
+      setData([...allDays])
     })
     //unmounts
     return () => { unsub() }
   }, [])
 
-  const handleRangeChange = e => {
-    console.log(e)
-  }
+  /* const handleRangeChange = e => {
+     console.log(e)
+   }
+   */
 
   return (
     <>
       <div>HomePage</div>
       <Calendar
-        onRangeChange={handleRangeChange}
+        //onRangeChange={handleRangeChange}
         localizer={localizer}
         events={data}
         style={{ height: 500, margin: "50px" }}
-        onSelectSlot={handleClick}
+        onSelectEvent={handleClick}
         selectable
       />
+      <ModalU show={show} setShow={setShow} modalUserInfo={modalUserInfo} />
     </>
   )
 }
